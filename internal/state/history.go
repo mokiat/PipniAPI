c6715fcdaa907d93cee8e979599144530aad1157
Momchil Atanasov <momchil.dev@gmail.com>
2023-09-04 16:58:18 +0300

    Relocate History code

--- internal/state/history.go
+++ internal/state/history.go
@@ -1,4 +1,4 @@
-package model
+package state
 
 import (
 	"github.com/mokiat/PipniAPI/internal/pds"
@@ -9,53 +9,6 @@ import (
 const (
 	historyCapacity = 25
 )
-
-type Change interface {
-	Apply()
-	Revert()
-}
-
-func FuncChange(apply, revert func()) Change {
-	return &funcChange{
-		apply:  apply,
-		revert: revert,
-	}
-}
-
-type funcChange struct {
-	apply  func()
-	revert func()
-}
-
-func (ch *funcChange) Apply() {
-	ch.apply()
-}
-
-func (ch *funcChange) Revert() {
-	ch.revert()
-}
-
-func CombinedChange(changes ...Change) Change {
-	return &combinedChange{
-		changes: changes,
-	}
-}
-
-type combinedChange struct {
-	changes []Change
-}
-
-func (c *combinedChange) Apply() {
-	for i := 0; i < len(c.changes); i++ {
-		c.changes[i].Apply()
-	}
-}
-
-func (c *combinedChange) Revert() {
-	for i := len(c.changes) - 1; i >= 0; i-- {
-		c.changes[i].Revert()
-	}
-}
 
 func NewHistory(eventBus *mvc.EventBus) *History {
 	return &History{
